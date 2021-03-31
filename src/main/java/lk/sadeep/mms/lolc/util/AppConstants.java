5e25d3f1d2f1f76c36cfa92d96655c86f81f4cfe
sadeeep <saddeepmihiranga@gmail.com>
2021-03-31 07:37:26 +0530

    Added TestGitUtil class docs.

--- src/main/java/lk/sadeep/mms/lolc/util/AppConstants.java
+++ src/main/java/lk/sadeep/mms/lolc/util/AppConstants.java
@@ -6,4 +6,6 @@ public class AppConstants
     public static final String DATE_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
 
     public static final String PASSWORD_STRING_V2 = "********";
+
+    // this comment done by anjula
 }
