48b8a243333c12aaf75ec9c90f5d809b18b5f212
sadeeep <saddeepmihiranga@gmail.com>
2021-03-31 06:37:54 +0530

    Added PASSWORD_STRING_V2 constant.

--- src/main/java/lk/sadeep/mms/lolc/util/AppConstants.java
+++ src/main/java/lk/sadeep/mms/lolc/util/AppConstants.java
@@ -4,4 +4,6 @@ public class AppConstants
 {
     public static final String PASSWORD_STRING = "********";
     public static final String DATE_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String PASSWORD_STRING_V2 = "********";
 }
